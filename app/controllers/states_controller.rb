3653930edb86a16610773e6316344456a8f1719a
Verbeck <jverbe3@gmail.com>
2018-01-18 23:25:55 -0500

    Adding maps to view page

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -2,9 +2,6 @@ class StatesController < ApplicationController
 
   def create
     @country = Country.find(params[:country_id])
-    p "here"
-    p params
-    p state_params[:state]
     @state = @country.states.create!(state_params[:state])
     redirect_to @country, :notice => "State added."
   end
